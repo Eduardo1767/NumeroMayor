055dcb53ad295313b1d3bbed8a8b069d0ce285b6
Eduardo1767 <61375130+Eduardo1767@users.noreply.github.com>
2020-05-04 22:27:45 -0500

    Add files via upload

---
+++ numeros.js
@@ -0,0 +1,25 @@
+var numero1 = prompt(" ingrese al Primer numero : ");
+var numero2 = prompt(" ingrese el segundo numero : ");
+var numero3 = prompt(" ingrese el tercer numero : ");
+
+numero1 = parseInt(numero1);
+numero2 = parseInt(numero2);
+numero3 = parseInt(numero3);
+
+
+    if (numero1 > numero2 && numero1 > numero3) {
+            document.write("El numero mayor es: " + numero1);
+        } else {
+
+        if (numero2 > numero1 && numero2 > numero3) {
+                document.write("El numero mayor es: " + numero2);
+            }
+            else {
+                if (numero3 > numero1 && numero3 > numero2) {
+                document.write("El numero mayor es: " + numero3);
+                }
+            }
+        }
+    
+
+
